11c03a3f444bd9ac9be1937237e45f4626acd251
Omar Khatib <omar@omarkhatib.co>
2023-05-02 11:44:11 +0200

    material test

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,25 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/checkbox/checkbox.js';
+import '@material/web/circularprogress/circular-progress.js';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div flex>omar</div>
+      <label>
+        Material 3<md-checkbox checked></md-checkbox>
+      </label>
+
+      <md-outlined-button label='Back'>test</md-outlined-button>
+      <md-filled-button label='Next'>test</md-filled-button>
+      <div>
+        <md-circular-progress progress={0.7} indeterminate />
+      </div>
       <div>
         <a href='https://vitejs.dev' target='_blank'>
           <img src={viteLogo} className='logo' alt='Vite logo' />
